10d4d9bdad3c00809e324ee8abff5fe55c018a40
Io <luke114@outlook.com>
2022-11-15 00:33:46 -0700

    rudamentary object system

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
 use std::io;
 
+use object::test_obj;
+
 mod reader;
 mod object;
 
 fn main() -> io::Result<()> {
-    reader::token_rpl()?;
+    // reader::token_rpl()?;
+    test_obj();
     Ok(())
 }
--- src/object.rs
+++ src/object.rs
@@ -1,8 +1,21 @@
-use std::{cell::{RefCell, Ref, RefMut}, rc::Rc};
+use std::{cell::{RefCell, Ref, RefMut}, rc::Rc, fmt::Debug};
 
 use machinate::Object;
 
-trait Object {
+pub fn test_obj() {
+    let minus = Symbol::new(String::from("-"));
+    let minuend = Int::new(21);
+    let subtrahend = Int::new(7);
+    let list = Cons::new(minus, Cons::new(minuend, Cons::new(subtrahend, Nil::new())));
+    println!(
+        "First element: {:?}\nSecond element: {:?}\nThird element: {:?}",
+        list.0.borrow().__car().unwrap(),
+        list.0.borrow().__cdr().unwrap().__car().unwrap(),
+        list.0.borrow().__cdr().unwrap().__cdr().unwrap().__car().unwrap(),
+    );
+}
+
+trait Object: Debug {
     fn __atom(&self) -> bool;
     fn __car(&self) -> Option<Ref<dyn Object>>;
     fn __cdr(&self) -> Option<Ref<dyn Object>>;
@@ -10,9 +23,26 @@ trait Object {
     fn __cdr_mut(&self) -> Option<RefMut<dyn Object>>;
 }
 
+#[derive(Debug)]
+struct ObjPtr(Rc<RefCell<dyn Object>>);
+
+#[derive(Debug)]
 struct Cons {
-    car: Rc<RefCell<dyn Object>>,
-    cdr: Rc<RefCell<dyn Object>>,
+    car: ObjPtr,
+    cdr: ObjPtr,
+}
+
+impl Cons {
+    fn new(car: ObjPtr, cdr: ObjPtr) -> ObjPtr {
+        ObjPtr(
+            Rc::new(
+                RefCell::new(
+                    Self {
+                        car,
+                        cdr,
+                    }
+                )))
+    }
 }
 
 impl Object for Cons {
@@ -21,22 +51,72 @@ impl Object for Cons {
     }
     
     fn __car(&self) -> Option<Ref<dyn Object>> {
-        Some(self.car.borrow())
+        Some(self.car.0.borrow())
     }
 
     fn __cdr(&self) -> Option<Ref<dyn Object>> {
-        Some(self.cdr.borrow())
+        Some(self.cdr.0.borrow())
     }
 
     fn __car_mut(&self) -> Option<RefMut<dyn Object>> {
-        Some(self.car.borrow_mut())
+        Some(self.car.0.borrow_mut())
     }
 
     fn __cdr_mut(&self) -> Option<RefMut<dyn Object>> {
-        Some(self.cdr.borrow_mut())
+        Some(self.cdr.0.borrow_mut())
     }
 }
 
-#[derive(Object)]
+#[derive(Debug, Object)]
 struct Nil;
 
+impl Nil {
+    fn new() -> ObjPtr {
+        ObjPtr( Rc::new( RefCell::new(Nil) ) )
+    }
+}
+
+#[derive(Debug, Object)]
+struct Int(i32);
+
+impl Int {
+    fn new(int: i32) -> ObjPtr {
+        ObjPtr( Rc::new( RefCell::new(Int(int)) ) )
+    }
+}
+
+#[derive(Debug, Object)]
+struct Float(f32);
+
+impl Float {
+    fn new(float: f32) -> ObjPtr {
+        ObjPtr( Rc::new( RefCell::new(Float(float)) ) )
+    }
+}
+
+#[derive(Debug, Object)]
+struct Char(char);
+
+impl Char {
+    fn new(ch: char) -> ObjPtr {
+        ObjPtr( Rc::new( RefCell::new(Char(ch)) ) )
+    }
+}
+
+#[derive(Debug, Object)]
+struct MString(String);
+
+impl MString {
+    fn new(mstr: String) -> ObjPtr {
+        ObjPtr( Rc::new( RefCell::new(MString(mstr)) ) )
+    }
+}
+
+#[derive(Debug, Object)]
+struct Symbol(String);
+
+impl Symbol {
+    fn new(mstr: String) -> ObjPtr {
+        ObjPtr( Rc::new( RefCell::new(Symbol(mstr)) ) )
+    }
+}
