5788f127165386af9ffb4630e49d95c6c5e191ce
LukeJohnson-io <lukejohnson10@suumail.net>
2022-12-04 20:01:56 -0700

    initial commit

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,5 @@
-use std::io;
 
-use object::tests::test_obj;
 
-mod reader;
-mod object;
-
-fn main() -> io::Result<()> {
-    reader::token_rpl()?;
-    test_obj();
-    Ok(())
+fn main()  {
+    
 }
--- src/object/types/mod.rs
+++
@@ -1,7 +0,0 @@
-pub mod cons;
-pub mod nil;
-pub mod int;
-pub mod float;
-pub mod mchar;
-pub mod mstring;
-pub mod symbol;
